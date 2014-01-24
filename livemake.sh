09b1045b2ea2e0a45e7f38a0e0fa62710616481e
William Culver <wculver@cedeon.co.uk>
2014-01-24 13:35:27 +0000

    Added Comment

--- livemake.sh
+++ livemake.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This file should hopefully generate the site on the live server
 VENVDIR=/home/walden/venv/bin
 BASEDIR=/home/walden/walden-obtm-weblog-checkout
 LIVEDIR=/var/www/walden/
